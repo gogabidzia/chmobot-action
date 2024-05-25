d4be778db7c5ab5da30c7153805d8651ddfadc56
Giorgi Tsirekidze <gogabidzia@gmail.com>
2024-05-25 17:16:56 +0400

    x

--- actions/chmobot-runner/entrypoint.sh
+++ actions/chmobot-runner/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
-echo "Hello $GITHUB_REPOSITORY with token $GITHUB_TOKEN" >> $GITHUB_OUTPUT
+echo "Hello $GITHUB_REPOSITORY with token $GITHUB_TOKEN"
 echo "time=$time" >> $GITHUB_OUTPUT
