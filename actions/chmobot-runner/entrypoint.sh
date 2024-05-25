7bcea673dfeb360017e6102640f9d006bca590b7
Giorgi Tsirekidze <gogabidzia@gmail.com>
2024-05-25 17:40:58 +0400

    x

--- actions/chmobot-runner/entrypoint.sh
+++ actions/chmobot-runner/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
+git config --global --add safe.directory /github/workspace
 git config user.email "905059+chmobot[bot]@users.noreply.github.com"
 git config user.name "chmobot[bot]"
 
