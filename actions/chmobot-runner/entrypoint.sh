f077b3adace0bf1e5670c1bc2a781b7499f2af12
Giorgi Tsirekidze <gogabidzia@gmail.com>
2024-05-25 18:00:22 +0400

    x

--- actions/chmobot-runner/entrypoint.sh
+++ actions/chmobot-runner/entrypoint.sh
@@ -7,12 +7,17 @@ git config --global --add safe.directory /github/workspace
 git config user.email "905059+chmobot[bot]@users.noreply.github.com"
 git config user.name "chmobot[bot]"
 
-git checkout -b chmobot/iss-1
+git checkout -b chmobot/iss-2
 
-echo "New update 1" >> README.md
+echo "New update 2" >> README.md
 
 git add .
 git commit -m "update from chmobot"
-git push origin chmobot/iss-1
+git push origin chmobot/iss-2
+
+curl -X POST \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  -d "{\"title\":\"Test\",\"body\":\"Test\",\"head\":\"chmobot/iss-2\",\"base\":\"main\"}" \
+  "https://api.github.com/repos/$GITHUB_REPOSITORY/pulls"
 
 echo "Done"
